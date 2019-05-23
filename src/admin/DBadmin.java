0fe7da79ba64222d982cb8104e48463d9e05bfb1
CHRITIANEDC <1053881227@qq.com>
2019-05-23 17:12:43 +0800

    change to a ok version

--- src/admin/DBadmin.java
+++ src/admin/DBadmin.java
@@ -2,7 +2,7 @@ package admin;
 
 public class DBadmin {
     public static String db_username = "root";//数据库用户名
-    public static String db_password = "ytz123";//数据库密码
+    public static String db_password = "thatisall0330";//数据库密码
     public static String db_name = "sdrb";//数据库名称
     public static String driverName = "com.mysql.cj.jdbc.Driver";//mysql数据库驱动程序
 }
