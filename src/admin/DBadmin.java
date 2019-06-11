13207bed155aa36e241936c13c59ffbdc30f1538
yusha-n <1816668710@qq.com>
2019-06-11 13:58:22 +0800

    delete some files should be ignored

--- src/admin/DBadmin.java
+++
@@ -1,8 +0,0 @@
-package admin;
-
-public class DBadmin {
-    public static String db_username = "root";//数据库用户名
-    public static String db_password = "381654";//数据库密码
-    public static String db_name = "sdrb";//数据库名称
-    public static String driverName = "com.mysql.cj.jdbc.Driver";//mysql数据库驱动程序
-}
